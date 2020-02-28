dd821c0593e1a002de2e7df75172644868b79f4e
Badlarry <alex.sinno@gmail.com>
2020-02-28 12:56:30 +0200

    Added UDPDataGram

--- TicketAnalyzer.ps1
+++ TicketAnalyzer.ps1
@@ -1,3 +1,19 @@
+#Send UDP Datagram By PeteGoo https://gist.github.com/PeteGoo/21a5ab7636786670e47c
+function Send-UdpDatagram
+{
+      Param ([string] $EndPoint, 
+      [int] $Port, 
+      [string] $Message)
+
+      $IP = [System.Net.Dns]::GetHostAddresses($EndPoint) 
+      $Address = [System.Net.IPAddress]::Parse($IP) 
+      $EndPoints = New-Object System.Net.IPEndPoint($Address, $Port) 
+      $Socket = New-Object System.Net.Sockets.UDPClient 
+      $EncodedText = [Text.Encoding]::ASCII.GetBytes($Message) 
+      $SendMessage = $Socket.Send($EncodedText, $EncodedText.Length, $EndPoints) 
+      $Socket.Close() 
+} 
+
 $LIST = klist
 $TimeMatches=@()
 $Regex = '\d\/(\d{1}|\d{2})\/(\d){4} \d.*\d'
@@ -16,8 +32,8 @@ foreach($Line in $LIST | Select-String  -Pattern '(Start Time)|(End Time)') {
 for($TicketCount -gt 0; $TimeCounter -lt $TicketCount; $TimeCounter+=2){
   $TimeDifference = New-TimeSpan $TimeMatches[$TimeCounter] $TimeMatches[$TimeCounter+1]
   if($TimeDifference.TotalHours -gt 10){
-    Write-Host "Warning suspicious TGT detection on" $env:computername -ForegroundColor Red
-    klist
+    $Message = Write-Host "Warning suspicious TGT detection on" $env:computername -ForegroundColor Red
+    Send-UdpDatagram -EndPoint 'Endpoint' -Port 'Port' -Message $Message
   }
   $TimeDifference
 }
